d7474b5814679de4e6f3105562cde3ef86301343
Risto Stevcev <risto1@gmail.com>
2014-04-26 17:16:27 -0700

    Updated readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='church_encoding',
-    version='0.1',
+    version='0.1.1',
     license='BSD',
     author='Risto Stevcev',
     author_email='risto1@gmail.com',
